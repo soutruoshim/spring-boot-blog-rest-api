e2e38352f74752f69f832204f1e1cc3dcd674fe8
soutruoshim <sout.rahim@gmail.com>
2023-06-25 08:37:33 +0700

    basic security

---
+++ src/main/java/com/srhdp/blogrestapi/config/SpringSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.srhdp.blogrestapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+@Configuration
+public class SpringSecurityConfig {
+//    @Bean
+//    public static PasswordEncoder passwordEncoder(){
+//        return new BCryptPasswordEncoder();
+//    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeHttpRequests((authorize) -> {
+                    authorize.anyRequest().authenticated();
+                }).httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
