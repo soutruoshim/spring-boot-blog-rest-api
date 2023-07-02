e1952173d59efe791c47d606a6053c0425de47f4
soutruoshim <sout.rahim@gmail.com>
2023-07-02 08:37:08 +0700

    start spring security

--- src/main/java/com/srhdp/blogrestapi/config/SecurityConfig.java
+++ src/main/java/com/srhdp/blogrestapi/config/SecurityConfig.java
@@ -1,11 +1,8 @@
 package com.srhdp.blogrestapi.config;
 
-import jakarta.servlet.DispatcherType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,10 +11,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-public class SpringSecurityConfig {
+public class SecurityConfig {
     @Bean
     public static PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
@@ -28,12 +24,8 @@ public class SpringSecurityConfig {
         http
                 .csrf().disable()
                 .authorizeHttpRequests((authorize) ->
-                        //authorize.anyRequest().authenticated()
-                        authorize.requestMatchers(HttpMethod.GET, "/api/**").permitAll()
-                                .requestMatchers("/api/posts/**").permitAll()
-                                .anyRequest().authenticated()
-
-                );
+                        authorize.anyRequest().authenticated()
+                ).httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
@@ -42,7 +34,7 @@ public class SpringSecurityConfig {
 
         UserDetails ramesh = User.builder()
                 .username("srhdp")
-                .password(passwordEncoder().encode("12345678"))
+                .password(passwordEncoder().encode("12345"))
                 .roles("USER")
                 .build();
 
