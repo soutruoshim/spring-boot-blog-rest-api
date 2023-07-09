617a68c660644ac3aa6ad367f113aefe55822743
soutruoshim <sout.rahim@gmail.com>
2023-07-09 08:22:50 +0700

    swagger change title with anotation

--- src/main/java/com/srhdp/blogrestapi/SpringbootBlogRestApiApplication.java
+++ src/main/java/com/srhdp/blogrestapi/SpringbootBlogRestApiApplication.java
@@ -1,11 +1,36 @@
 package com.srhdp.blogrestapi;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Spring Boot Blog App REST APIs",
+				description = "Spring Boot Blog App REST APIs Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Srhdp",
+						email = "srhdp@gmail.com",
+						url = "https://www.javaguides.net"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.javaguides.net/license"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Spring Boot Blog App Documentation",
+				url = "https://github.com/RameshMF/springboot-blog-rest-api"
+		)
+)
 public class SpringbootBlogRestApiApplication {
 
 	@Bean
