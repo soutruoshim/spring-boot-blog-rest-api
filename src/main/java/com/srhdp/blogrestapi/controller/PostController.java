d7472b80115586ccacf1fe4bd14763efc53a0b37
soutruoshim <sout.rahim@gmail.com>
2023-07-09 08:40:04 +0700

    custom api document

--- src/main/java/com/srhdp/blogrestapi/controller/PostController.java
+++ src/main/java/com/srhdp/blogrestapi/controller/PostController.java
@@ -4,7 +4,10 @@ import com.srhdp.blogrestapi.payload.PostDto;
 import com.srhdp.blogrestapi.payload.PostResponse;
 import com.srhdp.blogrestapi.service.PostService;
 import com.srhdp.blogrestapi.utils.AppConstants;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +18,9 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/posts")
+@Tag(
+        name = "CRUD REST APIs for Post Resource"
+)
 public class PostController {
     private PostService postService;
 
@@ -22,6 +28,17 @@ public class PostController {
         this.postService = postService;
     }
 
+    @Operation(
+            summary = "Create Post REST API",
+            description = "Create Post REST API is used to save post into database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Http Status 201 CREATED"
+    )
+    @SecurityRequirement(
+            name = "Bear Authentication"
+    )
     @SecurityRequirement(
             name = "Bear Authentication"
     )
@@ -38,6 +55,14 @@ public class PostController {
 //    ){
 //        return postService.getAllPosts(Integer.parseInt(""+pageNo), Integer.parseInt(""+pageSize));
 //    }
+    @Operation(
+            summary = "Get All Posts REST API",
+            description = "Get All Posts REST API is used to fetch all the posts from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
     @GetMapping
     public PostResponse getAllPosts(
             @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NO, required = false) int pageNo,
@@ -48,10 +73,19 @@ public class PostController {
         return postService.getAllPosts(Integer.parseInt(""+pageNo), Integer.parseInt(""+pageSize), sortBy, sortDir);
     }
 
+    @Operation(
+            summary = "Get Post By Id REST API",
+            description = "Get Post By Id REST API is used to get single post from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
     @GetMapping("/{id}")
     public ResponseEntity<PostDto> getPostById(@PathVariable(name = "id") long id){
         return ResponseEntity.ok(postService.getPostById(id));
     }
+
 
     @GetMapping("/category/{id}")
     public ResponseEntity<List<PostDto>> getPostsByCategory(@PathVariable("id") Long categoryId){
@@ -59,6 +93,17 @@ public class PostController {
         return ResponseEntity.ok(postDtos);
     }
 
+    @Operation(
+            summary = "update Post REST API",
+            description = "Update Post REST API is used to update a particular post in the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
+    @SecurityRequirement(
+            name = "Bear Authentication"
+    )
     @SecurityRequirement(
             name = "Bear Authentication"
     )
@@ -67,6 +112,14 @@ public class PostController {
     public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto, @PathVariable(name = "id") long id){
         return new ResponseEntity<>(postService.updatePost(postDto, id), HttpStatus.OK);
     }
+    @Operation(
+            summary = "Delete Post REST API",
+            description = "Delete Post REST API is used to delete a particular post from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
     @SecurityRequirement(
             name = "Bear Authentication"
     )
