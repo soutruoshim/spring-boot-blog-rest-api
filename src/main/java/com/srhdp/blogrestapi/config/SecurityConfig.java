aacce8145ba6756034b9261b274bc348efa471c1
soutruoshim <sout.rahim@gmail.com>
2023-07-09 08:19:50 +0700

    swagger config

--- src/main/java/com/srhdp/blogrestapi/config/SecurityConfig.java
+++ src/main/java/com/srhdp/blogrestapi/config/SecurityConfig.java
@@ -52,6 +52,8 @@ public class SecurityConfig {
                         authorize.requestMatchers(HttpMethod.GET, "/api/**").permitAll()
                                 //.requestMatchers(HttpMethod.GET, "/api/categories/**").permitAll()
                                 .requestMatchers("/api/auth/**").permitAll()
+                                .requestMatchers("/swagger-ui/**").permitAll()
+                                .requestMatchers("/v3/api-docs/**").permitAll()
                                 .anyRequest().authenticated()
 
                 ).exceptionHandling( exception -> exception
